38b1d4d08863600f6356a555e22afd6dbd3f6c21
sw1227 <32032108+sw1227@users.noreply.github.com>
2020-07-18 18:56:04 +0900

    add Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import Map2D from './views/Map2D';
 
 
-function App() {
+const routes = [
+  { path: "/", component: Home },
+  { path: "/map2d", component: Map2D },
+];
+
+function Home() {
   return (
-    <div>
-      Hello, world!
-    </div>
+    <ul>
+      {routes.map((r, i) => (
+        <li>
+          <Link to={r.path}>{r.component.name}</Link>
+        </li>
+      ))}
+    </ul>
   );
 }
 
-export default App;
+
+export default function AppRouter() {
+  return (
+    <Router>
+      {routes.map((r, i) => (
+        <Route path={r.path} exact component={r.component} key={i} />
+      ))}
+    </Router>
+  );
+}
---
+++ src/views/Map2D.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function Map2D() {
+
+  return (
+    <div>2d</div>
+  );
+}

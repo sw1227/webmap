f2d53de4e0975d0ea80c2711711137d03c072b70
sw1227 <32032108+sw1227@users.noreply.github.com>
2020-07-18 22:20:54 +0900

    add continuous zoom listener

--- src/views/Map2D.js
+++ src/views/Map2D.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+
+
+const zoomMin = 2;
+const zoomMax = 15;
+const zoomDelta = 1 / 120;
+
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "zoomdelta":
+      const newZoom = state.zoom + action.payload;
+      return {
+        zoom: Math.min(zoomMax, Math.max(zoomMin, newZoom))
+      };
+  }
+};
+
 
 export default function Map2D() {
+  // State: zoom level
+  const [state, dispatch] = useReducer(reducer, {
+    zoom: 10
+  });
+
+  // Event listener for mouse wheel
+  const handleWheel = event => {
+    event.preventDefault(); // Do not scroll
+
+    // Get wheel info. x,y: pixels in the element
+    const [x, y, delta] = [event.offsetX, event.offsetY, event.wheelDelta];
+    dispatch({type: "zoomdelta", payload: delta * zoomDelta});
+  };
+
+  // Register event listener
+  useEffect(() => {
+    const map = document.getElementById("map");
+    map.addEventListener("wheel", handleWheel);
+  }, []);
 
   return (
-    <div>2d</div>
+    <div>
+      <canvas id="map" width="600px" height="400px" style={{ border: "solid", margin: "20px" }} />
+      <div>zoom = {state.zoom}</div>
+    </div>
   );
 }
